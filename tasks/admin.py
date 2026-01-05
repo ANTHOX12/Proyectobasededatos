f456af6299dcad9c43a83c0852ee8b85f6c8bfea
ANTHOX12 <anthonyordo10@gmail.com>
2026-01-05 18:09:14 -0500

    fix: remove Task from admin and register CV models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Task
+from .models import (
+    DatosPersonales,
+    ExperienciaLaboral,
+    Reconocimiento,
+    CursoRealizado,
+    ProductoAcademico,
+)
 
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields=("created",)
-
-admin.site.register(Task, TaskAdmin)
+admin.site.register(DatosPersonales)
+admin.site.register(ExperienciaLaboral)
+admin.site.register(Reconocimiento)
+admin.site.register(CursoRealizado)
+admin.site.register(ProductoAcademico)
