348e36ffc0faed7b9c172fb06ece38667daa3105
ANTHOX12 <anthonyordo10@gmail.com>
2026-01-05 19:08:41 -0500

    feat: home loads CV models

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from .models import (
+    DatosPersonales,
+    CursoRealizado,
+    ExperienciaLaboral,
+    ProductoAcademico,
+    Reconocimiento,
+)
 
 def home(request):
-    return render(request, "home.html")
+    datos = DatosPersonales.objects.first()
+    cursos = CursoRealizado.objects.all()
+    experiencia = ExperienciaLaboral.objects.all()
+    productos = ProductoAcademico.objects.all()
+    reconocimientos = Reconocimiento.objects.all()
+
+    return render(request, "home.html", {
+        "datos": datos,
+        "cursos": cursos,
+        "experiencia": experiencia,
+        "productos": productos,
+        "reconocimientos": reconocimientos,
+    })
